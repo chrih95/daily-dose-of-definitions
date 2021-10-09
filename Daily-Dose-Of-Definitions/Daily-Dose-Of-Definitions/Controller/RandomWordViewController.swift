662c9ce6548c3caedabe7578ca3e001aebac8bd6
Christian Hawkes <chawkes77@gmail.com>
2021-10-09 18:57:29 -0400

    Add wordsAPI call to get new word

--- Daily-Dose-Of-Definitions/Daily-Dose-Of-Definitions/Controller/RandomWordViewController.swift
+++ Daily-Dose-Of-Definitions/Daily-Dose-Of-Definitions/Controller/RandomWordViewController.swift
@@ -55,11 +55,19 @@ class RandomWordViewController: UIViewController {
         button.translatesAutoresizingMaskIntoConstraints = false
         button.setBackgroundImage(UIImage(systemName: "arrow.clockwise.circle"), for: .normal)
         button.tintColor = .darkGray
+        button.addTarget(self, action: #selector(refreshButtonPressed), for: .touchUpInside)
         return button
     }()
+    
+    let headers = [
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": "90ea48b1d5msh84d3174c6860770p18bd31jsn806c0d897141"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refreshButtonPressed()
 
         setUpUI()
     }
@@ -101,6 +109,58 @@ class RandomWordViewController: UIViewController {
             refreshButton.widthAnchor.constraint(equalTo: refreshButton.heightAnchor),
         ])
     }
+    
+    @objc func refreshButtonPressed() {
+        refreshWord { word, error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            DispatchQueue.main.async { [weak self] in
+                self?.randomWordLabel.text = word?.word
+                self?.randomWordDescription.text = word?.results[0].partOfSpeech
+                self?.randomWordDefinition.text = word?.results[0].definition
+            }
+        }
+    }
+    
+    private func refreshWord(completion: @escaping (Word?, Error?) -> Void) {
+        guard let wordURL = URL(string: "https://wordsapiv1.p.rapidapi.com/words/?random=true") else {
+            print("Invalid URL")
+            return
+        }
+        
+        var urlRequest = URLRequest(url: wordURL)
+        urlRequest.httpMethod = "GET"
+        urlRequest.allHTTPHeaderFields = headers
+        
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            guard let data = data, error == nil else {
+                completion(nil, error)
+                return
+            }
+            
+            do {
+                let word = try JSONDecoder().decode(Word.self, from: data)
+                completion(word, error)
+                print(word)
+            }
+            catch {
+                print("Failed to convert \(error.localizedDescription)")
+                completion(nil, error)
+            }
+            
+        }.resume()
+    }
+}
+
+struct Word: Codable {
+    let word: String
+    let results: [WordResults]
+}
+
+struct WordResults: Codable {
+    let definition: String
+    let partOfSpeech: String
 }
 
 extension UIFont {
